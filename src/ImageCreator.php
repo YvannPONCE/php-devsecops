1ef4139b18c2c69d716270ca88acbc6cdcccb9c3
Mélanie Stanislas <melanie.stanislas@etu.univ-cotedazur.fr>
2024-11-17 16:15:47 +0100

    change doc of php code source

--- src/ImageCreator.php
+++ src/ImageCreator.php
@@ -12,17 +12,59 @@ use function imagefilledrectangle;
 use function imagepng;
 use function imagettftext;
 
+/**
+ * Class ImageCreator
+ *
+ * Cette classe est responsable de la création d'une image avec du texte dynamique.
+ * Elle utilise la librairie GD pour générer l'image et y dessiner du texte.
+ * 
+ * @package App
+ */
 class ImageCreator
 {
+    /**
+     * @var resource L'objet image GD créé
+     */
     protected $im;
+
+    /**
+     * @var int Couleur blanche pour l'image
+     */
     protected int $white;
+
+    /**
+     * @var int Couleur principale utilisée pour l'image
+     */
     protected int $yourColor;
+
+    /**
+     * @var int Deuxième couleur utilisée pour l'image
+     */
     protected int $yourColor2;
+
+    /**
+     * @var string Le texte principal à afficher sur l'image
+     */
     protected string $text;
+
+    /**
+     * @var string Le texte secondaire à afficher sur l'image
+     */
     protected string $text2;
+
+    /**
+     * @var string Le chemin de la police de caractères
+     */
     protected string $font;
 
-
+    /**
+     * ImageCreator constructor.
+     *
+     * @param array $yourColor La couleur principale
+     * @param array $yourColor2 La deuxième couleur
+     * @param string $text Le texte principal
+     * @param string $text2 Le texte secondaire
+     */
     public function __construct(
         array  $yourColor = [128, 128, 128],
         array  $yourColor2 = [60, 80, 57],
@@ -47,13 +89,25 @@ class ImageCreator
         $this->font = dirname(__DIR__) . '/public/font/consolas.ttf';
     }
 
-
+    /**
+     * Alloue une couleur à l'image
+     *
+     * @param array $rgb Le tableau de couleur (R, G, B)
+     * @return false|int Retourne la couleur allouée, ou false si erreur
+     */
     private function allocateColor(array $rgb): false|int
     {
         return imagecolorallocate($this->im, ...$rgb);
     }
 
-
+    /**
+     * Crée l'image et y ajoute les éléments graphiques
+     *
+     * Cette méthode dessine un rectangle sur l'image et y ajoute deux
+     * textes personnalisés avant de sauvegarder l'image.
+     *
+     * @return void
+     */
     public function createImage(): void
     {
         // Dessine un double rectangle
@@ -67,5 +121,12 @@ class ImageCreator
         // Sauvegarde l'image
         imagepng($this->im);
         imagedestroy($this->im);
+    }
+
+    // Aucune documentation pour cette méthode, elle devrait provoquer une erreur avec php-doc-check
+    private function helperFunction()
+    {
+        // Code sans docblock
+        return "Hello, world!";
     }
 }
